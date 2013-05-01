e42be257413fee57c5f7551dc20df93dcf487da2
Salman Gadit <salman.gadit@gmail.com>
2013-05-02 02:58:34 +0800

    DB config

--- model/db.js
+++ model/db.js
@@ -1,4 +1,12 @@
+var http = require ('http');  
 var mongoose = require( 'mongoose' );
+
+var uristring = 
+process.env.MONGOLAB_URI || 
+process.env.MONGOHQ_URL || 
+'mongodb://localhost/laughing-tyrion';
+
+var theport = process.env.PORT || 5000;
 
 var userSchema = new mongoose.Schema({
  first_name: String,
@@ -6,4 +14,12 @@ var userSchema = new mongoose.Schema({
 });
 mongoose.model( 'Users', userSchema );
 
-mongoose.connect( 'mongodb://localhost/laughing-tyrion' );
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, function (err, res) {
+  if (err) { 
+  console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
