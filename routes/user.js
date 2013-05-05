f4ed56da6106b9d17f3ffdb4b09b0b16043b6502
Salman Gadit <salman.gadit@gmail.com>
2013-05-06 04:57:06 +0800

    Params test

--- routes/user.js
+++ routes/user.js
@@ -6,3 +6,7 @@
 exports.list = function(req, res){
   res.send("respond with a resource");
 };
+
+exports.store = function(req, res){
+  console.log("User "+req.body.data.firstname + " " + req.body.data.lastname);
+};
