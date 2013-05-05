717630e624c08795b4e9e271ddde45e54e9bebc2
Salman Gadit <salman.gadit@gmail.com>
2013-05-06 06:50:58 +0800

    Data storing on mongo db

--- model/db.js
+++ model/db.js
@@ -4,7 +4,7 @@ var mongoose = require( 'mongoose' );
 var uristring = 
 process.env.MONGOLAB_URI || 
 process.env.MONGOHQ_URL || 
-'mongodb://localhost/laughing-tyrion';
+'mongodb://localhost/laughingtyrion';
 
 var theport = process.env.PORT || 5000;
 
@@ -12,7 +12,7 @@ var userSchema = new mongoose.Schema({
  name: String,
  id: String
 });
-mongoose.model( 'Users', userSchema );
+var User = mongoose.model( 'Users', userSchema );
 
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
@@ -23,3 +23,40 @@ mongoose.connect(uristring, function (err, res) {
   console.log ('Succeeded connected to: ' + uristring);
   }
 });
+
+var createUser = function(user,callback){
+    User.create(user, function(err, res){
+      if (err) {
+        console.log(err);
+        return callback(err, {});
+      }
+      console.log(res);
+      return callback(null,res);
+    });  
+};
+
+exports.findOrCreateUser = function(user, callback){
+	console.log(user);
+	console.log(callback);
+    User.findOne({id: user.id}, null, function(err, result){
+    	console.log('findOne result: ' + result);
+      if (err) {
+        console.log(err);
+        return callback(err, {});
+      } else if (!result) {
+        // If user does not exist, createuser.
+        return createUser(user, callback);
+      } else {
+        // If user exists, return user object.
+        return User.update({id: user.id}, user, function(err, result){
+          if (err) {
+            console.log(err);
+            return callback(err, {});
+          }
+          console.log(result);
+          return callback(null, result);
+        });
+      }
+  });
+  };
+
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@
 /*
  * GET users listing.
  */
+var db = require('../model/db')
 
 exports.list = function(req, res){
   res.send("respond with a resource");
@@ -11,5 +12,8 @@ exports.store = function(req, res){
 	console.log("Arrived");
  	console.log("User "+req.body.name + ", " + req.body.id);
 
- 	res.end("true");
+ 	db.findOrCreateUser(req.body, function(){
+ 		res.end("true");
+ 	});
+ 	
 };
